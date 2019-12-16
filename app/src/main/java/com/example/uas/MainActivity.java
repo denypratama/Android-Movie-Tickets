4bfad39067812529bb41e5c68f7ea8a652584002
fernanda <deny99067@gmail.com>
2019-12-16 13:52:48 +0700

    Splash, Slide, Fragment, Nofitication Bar
    Splash Selesai
    Picture Slide Selesai
    Nofitication Bar ganti warna Biru Selesai
    Fragment OTW

--- app/src/main/java/com/example/uas/MainActivity.java
+++ app/src/main/java/com/example/uas/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.uas;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
